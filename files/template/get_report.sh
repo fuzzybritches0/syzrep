cd1df52400ce0442b6ac452d6b1650d20ccec130
Kurt Manucredo <fuzzybritches0@gmail.com>
2021-05-05 23:48:48 +0000

    revert all

--- files/template/get_report.sh
+++ files/template/get_report.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
 
+filter_report() {
+	while read line; do
+		[ "${line/============================================}" != "${line}" ] && start_report=
+		[ "${line/'panic_on_warn set'}" != "${line}" ] && start_report=
+		[ "${line/KASAN}" != "${line}" ] && start_report="1"
+		[ "${line/UBSAN}" != "${line}" ] && start_report="1"
+		[ "${line/WARNING}" != "${line}" ] && start_report="1"
+		if [ "${start_report}" == "1" ]; then
+			[ "${line/'Creating new'}" == "${line}" ] && \
+			[ "${line/'filter on device'}" == "${line}" ] && \
+			[ "${line/'entered blocking state'}" == "${line}" ] && \
+			[ "${line/'entered forwarding state'}" == "${line}" ] && \
+			[ "${line/'link becomes ready'}" == "${line}" ] && echo ${line}
+		fi
+	done
+}
+
+cut_timestamps() {
+	while read line; do
+		echo "${line:20}"
+	done
+}
+
 ./reproduce.sh &
-./run.sh
+./run.sh | ansi2txt > ./report_full.txt
+cat ./report_full.txt | filter_report | cut_timestamps > ./report.txt
 ./decode_stacktrace.sh
-rm report_raw.txt
+cat ./report_decoded.txt | ansi2txt > report.txt
+rm ./report_decoded.txt
+rm ./report_full.txt
+
 exit 0

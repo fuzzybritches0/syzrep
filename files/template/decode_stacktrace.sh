8309cfabf246d86a356da0dc27ca1776bb273e33
Kurt Manucredo <fuzzybritches0@gmail.com>
2021-05-11 18:50:33 +0000

    refactoring/cleanup

--- files/template/decode_stacktrace.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-[ ! -e "./linux/scripts/decode_stacktrace.sh" ] && echo "./linux/scripts/decode_stacktrace.sh not found!" && exit 1
-
-cat ./report${1}.txt | ./linux/scripts/decode_stacktrace.sh \
-	./linux/vmlinux . > ./report_decoded${1}.txt
-
